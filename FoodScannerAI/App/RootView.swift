d4ec98496f2fe9486ada6d205cf8bd75ea451f41
Ngoni AppleDev <ngonikatsidzira@icloud.com>
2026-07-11 14:15:01 +0200

    feat: create project foundation

--- FoodScannerAI/App/RootView.swift
+++ FoodScannerAI/App/RootView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  RootView.swift
 //  FoodScannerAI
 //
 //  Created by Ngoni Katsidzira  on 11/7/2026.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RootView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    RootView()
 }
